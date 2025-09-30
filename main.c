11612658430ea0c7d263d03223fdcadfb1d647b4
yorick-1101625 <1101625@hr.nl>
2025-09-30 11:22:18 +0200

    Create player struct and first movement

--- main.c
+++ main.c
@@ -1,19 +1,39 @@
-include "raylib.h"
+#include "raylib.h"
 
 int main() {
     
+    typedef struct {
+        Vector2 position;
+        Vector2 size;
+        float speed;
+    } player_t;
+    
+    
     InitWindow(800, 450, "Ctrl-C");
     
-    SetTargetFPS(60);
+    player_t player = {
+        .position = { 0, 0 },
+        .size = { 50, 50 },
+        .speed = 30.0f
+    };
+    
+    Vector2 position = { 0, 0 };
+    Vector2 size = { 50, 50 };
     
     while(!WindowShouldClose()) {
+        float dt = GetFrameTime();
         
         BeginDrawing();
         
         ClearBackground(RAYWHITE);
         
-        EndDrawing();
+        if (IsKeyDown('D')) {
+            player.position.x += player.speed * dt;
+        }
         
+        DrawRectangleV(player.position, player.size, RED);
+        
+        EndDrawing();
     }
     
     CloseWindow();
