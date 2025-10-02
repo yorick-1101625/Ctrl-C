b38e215e87bc689fa0825ee46083e1ab966a081b
yorick-1101625 <1101625@hr.nl>
2025-10-02 19:46:22 +0200

    Implement movement boundaries

--- player.c
+++ player.c
@@ -1,5 +1,6 @@
 #include "raylib.h"
 #include "player.h"
+#include "constants.h"
 
 void player_move(player_t *p, float dt);
 void player_check_input(player_t *p);
@@ -17,7 +18,22 @@ void player_draw(player_t *p) {
 void player_move(player_t *p, float dt) {
     
     p->position.x += p->speed * p->direction.x * dt ;
-    p->position.y += p->speed * p->direction.y * dt;   
+    p->position.y += p->speed * p->direction.y * dt;  
+    
+    // Set movement boundaries
+    // TODO: Ignore transparent pixels
+    if (p->position.x > SCREEN_WIDTH) {
+        p->position.x = SCREEN_WIDTH;
+    }
+    else if (p->position.x < 0) {
+        p->position.x = 0;
+    }
+    if (p->position.y > SCREEN_HEIGHT) {
+        p->position.y = SCREEN_HEIGHT;
+    }
+    else if (p->position.y < 0) {
+        p->position.y = 0;
+    }
 }
 
 void player_check_input(player_t *p) {
